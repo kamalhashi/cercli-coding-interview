12979d24781dee9e4ed723970947e3cab7697fe4
Kamal Hashi <kamalweheliye@gmail.com>
2024-05-30 20:38:44 +0400

    update read.me file

--- src/main/kotlin/com/cercli/employeeservice/service/EmployeeService.kt
+++ src/main/kotlin/com/cercli/employeeservice/service/EmployeeService.kt
@@ -12,11 +12,31 @@ import java.util.UUID
 @Service
 class EmployeeService(val employeeRepository: EmployeeRepository) {
 
+
+    /**
+     * Retrieves an employee by their ID.
+     *
+     * This method fetches the employee by employee ID. If the employee
+     * with the specified ID does not exist, a ResourceNotFoundException is thrown.
+     *
+     * @param employeeId the ID of the employee to retrieve.
+     * @return the employee entity corresponding to the provided ID.
+     * @throws ResourceNotFoundException if no employee is found with the specified ID.
+     */
     fun getEmployeeById(employeeId: UUID): Employee {
         val employee = employeeRepository.findById(employeeId)
             .orElseThrow { ResourceNotFoundException("Employee not found") }
         return employee
     }
+
+    /**
+     * Creates a new employee.
+     *
+     * This method creates a new employee entity based on the information provided in the
+     * createEmployeeDto parameter.
+     * @param createEmployeeDto the data transfer object containing the details of the new employee.
+     * @return the newly created employee entity.
+     */
 
     @Transactional
     fun createEmployee(createEmployeeDto: CreateEmployeeDto): Employee {
--- src/main/kotlin/com/cercli/employeeservice/service/TimeOffRequestService.kt
+++ src/main/kotlin/com/cercli/employeeservice/service/TimeOffRequestService.kt
@@ -5,6 +5,7 @@ import com.cercli.employeeservice.entity.Employee
 import com.cercli.employeeservice.entity.RequestCategory
 import com.cercli.employeeservice.entity.TimeOffRequest
 import com.cercli.employeeservice.enums.RequestCategoryName
+import com.cercli.employeeservice.exception.OverlappingTimeOffRequestException
 import com.cercli.employeeservice.exception.ResourceNotFoundException
 import com.cercli.employeeservice.repository.EmployeeRepository
 import com.cercli.employeeservice.repository.RequestCategoryRepository
@@ -24,6 +25,17 @@ class TimeOffRequestService(
         return timeOffRequestRepository.findAll()
     }
 
+    /**
+     * Creates a new time off request for an employee.
+     *
+     * This method validates that the end date is after or equal to the start date, retrieves the employee and request category,
+     * checks for overlapping requests with different categories, and then saves the time off request.
+     *
+     * @param createTimeOffRequestDto the data transfer object containing the details of the time off request to be created.
+     * @return the saved TimeOffRequest entity.
+     * @throws ResourceNotFoundException if the employee or request category is not found.
+     * @throws OverlappingTimeOffRequestException if there is an overlapping time-off request with a different category.
+     */
     @Transactional
     fun createTimeOffRequestDto(createTimeOffRequestDto: CreateTimeOffRequestDto): TimeOffRequest {
         // Validate that the end date is after or equal to the start date
@@ -41,7 +53,7 @@ class TimeOffRequestService(
         // Check for overlapping requests with different categories
         hasOverlappingRequests(employee, createTimeOffRequestDto, requestCategory)
         if (hasOverlappingRequests(employee, createTimeOffRequestDto, requestCategory)) {
-            throw IllegalArgumentException("Overlapping time-off request with a different category exists")
+            throw OverlappingTimeOffRequestException("Overlapping time-off request with a different category exists")
         }
 
         //save the request
@@ -50,6 +62,18 @@ class TimeOffRequestService(
         return timeOffRequestRepository.save(timeOffRequest)
     }
 
+
+    /**
+     * Checks if there are overlapping time-off requests for the given employee.
+     *
+     * This method iterates through the employee's existing time-off requests and checks if there is an overlap
+     * with the new time-off request dates. If an overlap is found then we check whether existing time-off-request is "Work Remote"
+     *
+     * @param employee the employee whose time-off requests are being checked.
+     * @param createTimeOffRequestDto the data transfer object containing the details of the new time off request.
+     * @param requestCategory the request category of the new time off request.
+     * @return true if there is an overlapping time-off request with a different category, false otherwise.
+     */
     private fun hasOverlappingRequests(
         employee: Employee,
         createTimeOffRequestDto: CreateTimeOffRequestDto,
@@ -72,6 +96,16 @@ class TimeOffRequestService(
         return false // No overlap found
     }
 
+    /**
+     * Employee can request "Annual Leave " while  "Working Remotely"
+     *
+     * This method compares the existing and new request category names to determine if one is "Work Remotely" and
+     * the other is "Annual Leave".
+     *
+     * @param existingCategoryName the name of the existing request category.
+     * @param newCategoryName the name of the new request category.
+     * @return true if the existing category is "Work Remotely" and the new category is "Annual Leave", false otherwise.
+     */
     private fun isWorkRemotelyAndAnnualLeave(existingCategoryName: String, newCategoryName: String): Boolean {
         val existingCategoryEnum = RequestCategoryName.fromCategoryName(existingCategoryName)
         val newCategoryEnum = RequestCategoryName.fromCategoryName(newCategoryName)
